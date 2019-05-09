803efdf634228ff99dc81a950207dbd5bb4922a3
Patrick Einatz <patrickeinatz@pop-os.localdomain>
2019-05-09 21:06:44 +0200

    added login page template

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/lgn", name="login")
+     */
+    public function index()
+    {
+        return $this->render('login/login.html.twig', [
+            'title' => 'Log in!',
+        ]);
+    }
+}
