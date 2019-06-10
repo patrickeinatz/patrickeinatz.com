d179b89ac142e5147f1420629dbf0919d8394333
Patrick Einatz <patrickeinatz@pop-os.localdomain>
2019-06-10 13:18:16 +0200

    init search field for backend comment manager

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentAdminController extends AbstractController
@@ -11,13 +12,15 @@ class CommentAdminController extends AbstractController
     /**
      * @Route("/backend/comment", name="be_comment")
      */
-    public function index(CommentRepository $repository)
+    public function index(CommentRepository $repository, Request $request)
     {
-        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
+        $q = $request->query->get('q');
+
+        $comments = $repository->findAllWithSearch($q);
 
         return $this->render('comment_admin/index.html.twig', [
             'title' => 'Comment Manager',
-            'comments' => $comments
+            'comments' => $comments,
         ]);
     }
 }
