ce328582dc6aff14aa8091bc0d308edfa7029402
Patrick Einatz <patrickeinatz@pop-os.localdomain>
2019-06-10 13:39:47 +0200

    optimized comment manager search

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -23,14 +23,16 @@ class CommentRepository extends ServiceEntityRepository
     /**
      * @param string|null $term
      *
-     * @return Comment []
+     * @return Comment[]
      */
     public function findAllWithSearch(?string $term)
     {
-        $qb = $this->createQueryBuilder('c');
+        $qb = $this->createQueryBuilder('c')
+            ->innerJoin('c.article', 'a')
+            ->addSelect('a');
 
         if($term){
-            $qb->andWhere('c.content LIKE :term OR c.authorName LIKE :term')
+            $qb->andWhere('c.content LIKE :term OR c.authorName LIKE :term OR a.title LIKE :term')
                 ->setParameter('term', '%'.$term.'%')
             ;
         }
