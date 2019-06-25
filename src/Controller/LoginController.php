aa89f1d1d647230955a9c8634370d3c7ad51da49
Patrick Einatz <patrickeinatz@pop-os.localdomain>
2019-06-25 22:00:26 +0200

    init simple registration

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
@@ -26,12 +31,49 @@ class LoginController extends AbstractController
         ]);
     }
 
-
     /**
      * @Route("/logout", name="logout")
      */
     public function logout()
     {
         throw new \Exception('will be intercepted before getting here');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder,
+        GuardAuthenticatorHandler $guardHandler,
+        LoginFormAuthenticator $formAuthenticator
+        )
+    {
+        // TODO - use symfony forms & validation
+
+        if($request->isMethod(('POST'))){
+            $user = new User();
+            $user->setEmail($request->request->get('email'));
+            $user->setFirstName('Mystery');
+            $user->setPassword($passwordEncoder->encodePassword(
+                $user,
+                $request->request->get('password')
+            ));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $formAuthenticator,
+                'main'
+            );
+        }
+
+        return $this->render('login/register.html.twig', [
+            'title' => 'Registration'
+        ]);
     }
 }
